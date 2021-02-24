d88f6b4fb8a5c1ae28d07cf79967004ba72122d1
isosic <ivan.sosic@asseco-see.hr>
2021-02-24 17:05:10 +0100

    added todos, updated gradle dependencies

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,4 +3,12 @@ package com.udacity.asteroidradar.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    //TODO get image of the day via retrofit
+    //TODO get asteroid data via retrofit
+    //TODO save data in the database
+    //TODO display data via recycler view
+    //TODO on click of the recycler view item, navigate to the fragment details
+    //TODO pass data with safeargs to the fragment detail
+
 }
