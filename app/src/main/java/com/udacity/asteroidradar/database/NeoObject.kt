49fff55ea7e03ce3baee23d1c6545608c22ce6d9
isosic <ivan.sosic@asseco-see.hr>
2021-02-26 21:57:17 +0100

    created database object

---
+++ app/src/main/java/com/udacity/asteroidradar/database/NeoObject.kt
@@ -0,0 +1,30 @@
+package com.udacity.asteroidradar.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "neo_object_table")
+data class NeoObject(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    var neoId: Double,
+
+    @ColumnInfo(name = "absolute_magnitude")
+    var absoluteMagnitude: Double,
+
+    @ColumnInfo(name = "estimated_diameter_max")
+    var estimatedDiameterMax : Double,
+
+    @ColumnInfo(name = "is_potentially_hazardous_asteroid")
+    var potentiallyHazardous : Boolean,
+
+    //close_approach_data -> relative_velocity -> kilometers_per_second
+    @ColumnInfo(name = "relative_velocity")
+    var relativeVelocity : Double,
+
+    //close_approach_data -> miss_distance -> astronomical
+    @ColumnInfo(name = "miss_distance")
+    var missDistance: Double
+
+)
