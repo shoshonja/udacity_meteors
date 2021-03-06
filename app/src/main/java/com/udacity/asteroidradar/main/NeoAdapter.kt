284e35119155c129708af12c24db4936c73e22cb
isosic <ivan.sosic@asseco-see.hr>
2021-03-06 10:05:23 +0100

    removed placeholder listview item, fixed layout issues with neo list view

--- app/src/main/java/com/udacity/asteroidradar/main/NeoAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/NeoAdapter.kt
@@ -3,8 +3,8 @@ package com.udacity.asteroidradar.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.R
@@ -19,7 +19,7 @@ class NeoAdapter : RecyclerView.Adapter<NeoAdapter.NeoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NeoViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.random, parent, false) as  TextView
+        val view = layoutInflater.inflate(R.layout.list_item_neo, parent, false)
 
         return NeoViewHolder(view)
     }
@@ -29,22 +29,25 @@ class NeoAdapter : RecyclerView.Adapter<NeoAdapter.NeoViewHolder>() {
         val date = data[position].closeApproachDate
         val isHazardous = data[position].isPotentiallyHazardous
 
-//        holder.codeName.text = name
-//        holder.approachDate.text = date
-        holder.textView.text = name
+        holder.codeName.text = name
+        holder.approachDate.text = date
+        holder.hazardousIcon.setImageResource(
+            if (isHazardous) {
+                R.drawable.ic_status_potentially_hazardous
+            } else {
+                R.drawable.ic_status_normal
+            }
+        )
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-//    class NeoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val codeName = itemView.findViewById(R.id.list_neo_codename) as TextView
-//        val approachDate = itemView.findViewById(R.id.list_neo_approach_date) as TextView
-//    }
-
-    class NeoViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView){
-
+    class NeoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val codeName = itemView.findViewById(R.id.list_neo_codename) as TextView
+        val approachDate = itemView.findViewById(R.id.list_neo_approach_date) as TextView
+        val hazardousIcon = itemView.findViewById(R.id.list_neo_is_hazardous) as ImageView
     }
 
 }
